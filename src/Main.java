4528bda2bf928b7d7d3645d6474685f2e1e29e57
Ibrokhimm0752 <Ibrokhimm0752@hstat.org>
2025-04-06 19:45:51 -0400

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,87 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static ArrayList<String> list = new ArrayList<>();
+    private static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String menuOption;
+        do {
+            displayMenu();
+            menuOption = SafeInput.getRegExString(in, "Choose an option [A, D, I, P, Q]", "[AaDdIiPpQq]");
+            switch (menuOption.toUpperCase()) {
+                case "A":
+                    addItem();
+                    break;
+                case "D":
+                    deleteItem();
+                    break;
+                case "I":
+                    insertItem();
+                    break;
+                case "P":
+                    printList();
+                    break;
+                case "Q":
+                    if (SafeInput.getYNConfirm(in, "Are you sure you want to quit? ")) {
+                        System.out.println("Exiting program.");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid option. Try again.");
+            }
+        } while (true);
+    }
+
+    private static void displayMenu() {
+        System.out.println("\nCurrent List:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i));
+        }
+        System.out.println("\nMenu Options:");
+        System.out.println("A - Add an item to the list");
+        System.out.println("D - Delete an item from the list");
+        System.out.println("I - Insert an item into the list");
+        System.out.println("P - Print the list");
+        System.out.println("Q - Quit the program");
+    }
+
+    private static void addItem() {
+        String newItem = SafeInput.getNonZeroLenString(in, "Enter the item to add");
+        list.add(newItem);
+        System.out.println("Item added.");
+    }
+
+    private static void deleteItem() {
+        if (list.isEmpty()) {
+            System.out.println("The list is empty, nothing to delete.");
+            return;
+        }
+        int itemNumber = SafeInput.getRangedInt(in, "Enter the item number to delete:", 1, list.size());
+        list.remove(itemNumber - 1);
+        System.out.println("Item deleted.");
+    }
+
+    private static void insertItem() {
+        if (list.isEmpty()) {
+            System.out.println("List is empty, inserting at the first position.");
+        }
+        int position = SafeInput.getRangedInt(in, "Enter the position to insert the item " + (list.size() + 1), 1, list.size() + 1);
+        String newItem = SafeInput.getNonZeroLenString(in, "Enter the item to insert");
+        list.add(position - 1, newItem);
+        System.out.println("Item inserted.");
+    }
+
+    private static void printList() {
+        System.out.println("\nCurrent List:");
+        if (list.isEmpty()) {
+            System.out.println("The list is currently empty.");
+        } else {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println((i + 1) + ". " + list.get(i));
+            }
+        }
     }
 }
